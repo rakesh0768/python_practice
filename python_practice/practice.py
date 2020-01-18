c8ebe33457bf6d3d7c6ffa31892271435ac35f0d
rakesh0768 <rakeshsoni0768@gmail.com>
2020-01-18 13:37:18 +0530

    new commit

---
+++ python_practice/practice.py
@@ -0,0 +1,8 @@
+print("hello")
+
+mylist = [1, 2, 3, 4, 5]
+
+# Create a for loop that iterates through all the items of the following list and prints out the item if the item is greater than 2.
+for i in mylist:
+    if i>2:
+        print(i)
